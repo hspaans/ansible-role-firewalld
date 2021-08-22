d0b5da971d839f1d0166dbc1fb33ca281d985cb6
Hans Spaans <hans@dailystuff.nl>
2021-08-22 09:50:22 +0200

    Updating scenario

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,6 +5,7 @@ import pytest
 
 @pytest.mark.parametrize("pkg", ["firewalld"])
 def test_pkg_installed(host, pkg):
+    """Test if package installed."""
     package = host.package(pkg)
 
     assert package.is_installed
